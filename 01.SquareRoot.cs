c7baed86e7f3c838600931e46abc7a3990a41398
Asen Todorov <astodorov@outlook.com>
2015-06-07 12:12:54 +0300

    OOP Homework 02
    Exception Handling

---
+++ 01.SquareRoot.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/// Write a program that reads an integer number and calculates and prints its square root. If the number is invalid or negative, 
+/// print "Invalid number". In all cases finally print "Good bye". Use try-catch-finally.
+
+class SquareRoot
+{
+    static void Main()
+    {
+        CalculateSquareRoot();
+    }
+
+    public static void CalculateSquareRoot()
+    {
+        try
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException("number", "Number shoud be positive integer number!");
+            }
+            Console.WriteLine("Square root of {0} is {1}", number, Math.Sqrt(number));
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid number!");
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Invalid number!");
+        }
+        finally
+        {
+            Console.WriteLine("Good bye!");
+        }
+    }
+}
---
+++ 02.EnterNumbers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+/// Write a method ReadNumber(int start, int end) that enters an integer number in given range [start…end]. If an invalid number or 
+/// non-number text is entered, the method should throw an exception. Using this method write a program that enters 10 numbers: 
+/// a1, a2, … a10, such that 1 < a1 < … < a10 < 100. If the user enters an invalid number, let the user to enter again.
+
+class EnterNumbers
+{
+    static void Main()
+    {
+        int start = 1;
+        int end = 100;
+        int[] numbers = new int[10];
+
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = ReadNumber(start+1, end-(numbers.Length-i));
+            start = numbers[i];
+        }
+        Console.WriteLine("Your numbers are:");
+        Console.WriteLine(string.Join(",", numbers));
+
+    }
+    private static int ReadNumber(int start, int end)
+    {
+        try
+        {
+            string userInput = Console.ReadLine();
+            int num;
+            bool successfullyParsed = int.TryParse(userInput, out num);
+            if (!successfullyParsed || num < start || num > end)
+            {
+                throw new ArgumentException("num", "Invalid number!");
+            }
+            return num;
+        }
+        catch (ArgumentException)
+        {
+            Console.WriteLine("You have entered an incorrect number. Please enter a new number.");
+            return ReadNumber(start, end);
+        }
+    }
+}
